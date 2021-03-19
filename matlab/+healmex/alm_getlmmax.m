1fbb8465de482b2b4c972f9f4b69439bce172e5e
Justin Willmert <justin@willmert.me>
2021-03-19 10:26:25 -0400

    Eliminate subfunctions in simple calculation function

--- matlab/+healmex/alm_getlmmax.m
+++ matlab/+healmex/alm_getlmmax.m
@@ -11,30 +11,26 @@ function [lmax, mmax] = alm_getlmmax(alms, lmax, mmax)
   end
 
   if isempty(lmax)
+    nel = size(alms, 1);
     if isempty(mmax)
-      lmax = get_lmax1(size(alms,1));
+      % Translated from healpy's _sphtools.pyx source
+      %function lmax=get_lmax1(nel)
+      lmax = (sqrt(1 + 8*nel) - 3) / 2;
+      if lmax ~= floor(lmax)
+        throwAsCaller(MException('healmex:alm_getlmmax:sizeError', 'Could not infer lmax'));
+      end
+      %end
       mmax = lmax;
     else
-      lmax = get_lmax2(size(alms,1), mmax);
+      % Translated from healpy's _sphtools.pyx source
+      %function lmax=get_lmax2(nel, mmax)
+      lmax = (2*nel + mmax^2 - mmax - 2) / (2*mmax + 2);
+      if lmax ~= floor(lmax)
+        throwAsCaller(MException('healmex:alm_getlmmax:sizeError', 'Could not infer lmax'));
+      end
+      %end
     end
   elseif isempty(mmax)
     mmax = lmax;
-  end
-
-end
-%
-% Translated from healpy's _sphtools.pyx source
-function lmax=get_lmax1(nel)
-  lmax = (sqrt(1 + 8*nel) - 3) / 2;
-  if lmax ~= floor(lmax)
-    error('Could not infer lmax');
-  end
-end
-
-% Translated from healpy's _sphtools.pyx source
-function lmax=get_lmax2(nel, mmax)
-  lmax = (2*nel + mmax^2 - mmax - 2) / (2*mmax + 2);
-  if lmax ~= floor(lmax)
-    error('Could not infer lmax');
   end
 end
