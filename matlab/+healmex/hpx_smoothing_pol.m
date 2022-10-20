d789ca0cbdf19fd82821d2702094749c1c1a076b
Dominic Beck <dbeck@holybicep01.rc.fas.harvard.edu>
2022-10-20 11:44:20 -0400

    Q,U smoothing

---
+++ matlab/+healmex/hpx_smoothing_pol.m
@@ -0,0 +1,3 @@
+function [mapQ,mapU] = hpx_smoothing(mapQ, mapU, fle, flb, order, lmax, mmax, mmin, nside, rwghts, niter)  
+  [mapQ,mapU] = libhealmex(int64(72), int64(nside), char(order), double(mapQ), double(mapU), double(fle), double(flb), int32(lmax), int32(mmax), int32(mmin), double(rwghts), int32(niter));
+end
